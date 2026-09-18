1599a43b02e4a7fcaa97ae949d8c4cc036b9d59f
jhorfan <jhorfanvega@gmail.com>
2026-09-18 09:52:35 -0500

    variables

--- src/saludo.java
+++ src/saludo.java
@@ -1,6 +1,7 @@
 public class saludo {
     public static void main(String[] args) {
         System.out.printf("hola mundo");
+
     }
 
 }
---
+++ src/variables.java
@@ -0,0 +1,16 @@
+public class variables {
+    public static void main(String[] args) {
+        String valor = "111";
+        int costo = 300;
+        double valor2 = 2.23;
+        char letra1 = 'B';
+        boolean esTudiante = false;
+
+        System.out.println(valor);
+        System.out.println(costo);
+        System.out.println(valor2);
+        System.out.println(letra1);
+        System.out.println(esTudiante);
+
+    }
+}
